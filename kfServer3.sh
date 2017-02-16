224b102c543413cbaab1315d4a40415d7d4bfd6d
michael moriarty <michael.moriarty@maersk.com>
2017-02-16 13:33:34 +0000

    updates

--- kfServer3.sh
+++ kfServer3.sh
@@ -4,6 +4,6 @@ docker run -d \
    --net=host \
    --name=kafka-3 \
    -e KAFKA_ZOOKEEPER_CONNECT=10.9.3.4:22181,10.9.3.5:32181,10.9.3.6:42181 \
-   -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:49092 \
+   -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://10.9.3.6:49092 \
    confluentinc/cp-kafka:3.1.2
 
